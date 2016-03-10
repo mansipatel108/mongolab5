57869527b65f333c3d06a77440728b9700e2eb7f
Mansi <mpatel.y@gmail.com>
2016-03-10 14:36:40 -0500

    final commit

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+"use strict";
+var mongoose = require('mongoose');
+// DEFINE THE OBJECT SCHEMA
+var userSchema = new mongoose.Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Username is required'
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Password is required'
+    }
+});
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+exports.User = mongoose.model('User', userSchema);
+
+//# sourceMappingURL=user.js.map
